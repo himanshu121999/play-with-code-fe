3523fa5429f2239bbab1e85c08e69a6d461d01bb
Himanshu Jain <himanshu0121999+1@gmail.com>
2024-10-19 13:47:29 +0530

    vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/play-with-code-FE/",
+  base: "/play-with-code/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["@tabler/icons-react"],
